1eb05ec8a4762bb0af02f2c6cbf236936e7a03c1
Jae Yong Kim <kim900180@gmail.com>
2018-08-15 15:52:49 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   # projects: kickstarter.css("li.projejct.grid_4")
+  #title: project.css("h2.bbcard_name strong a").text
+  #project.css("div.project-thumbnail a img").attribute("src").value
   binding.pry
 
 end
